3229dbdaa3c24974a8e531daaa62d2cd73269576
Ravi-2504 <ravi.vignan2504@gmail.com>
2016-05-30 14:28:19 +0530

    QD-1 Hibernate Helloworld using XML

---
+++ src/main/java/com/hibernate/helloworld/Main.java
@@ -0,0 +1,53 @@
+package com.hibernate.helloworld;
+
+import java.sql.Date;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		List employees = list();
+		System.out.println("Total Employees: " + employees.size());
+		
+		Employee empl = new Employee("Ravi", "Kumar", new Date(System.currentTimeMillis()), "911");
+		empl = save(empl);
+		System.out.printf("%s %s \n", empl.getFirstname(), empl.getLastname());
+		
+	}
+	
+	
+
+	private static List list() {
+		SessionFactory sf = HibernateUtil.getSessionFactory();
+		Session session = sf.openSession();
+
+		List employees = session.createQuery("from Employee").list();
+		session.close();
+		return employees;
+	}
+	
+	private static Employee save(Employee employee) {
+		SessionFactory sf = HibernateUtil.getSessionFactory();
+		Session session = sf.openSession();
+
+		session.beginTransaction();
+
+		int id = (int) session.save(employee);
+		employee.setId(id);
+		
+		session.getTransaction().commit();
+		
+		session.close();
+
+		return employee;
+	}
+
+	
+	
+}
